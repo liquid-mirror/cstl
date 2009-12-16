cb0d549380dae6a1c2ae55686d70e0d63f3f3268
katono <katono>
2009-12-16 14:54:49 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/cstl/trunk@294 ff0e48b6-bc2a-0410-a31c-c11babe924ff

--- test/hoge.h
+++ test/hoge.h
@@ -11,17 +11,8 @@ typedef struct Hoge {
 #define HOGE_COMP(x, y)	 	strcmp((x).key, (y).key)
 #define HOGEP_COMP(x, y)	strcmp((x)->key, (y)->key)
 
-size_t HOGE_HASH(Hoge x)
-{
-	/* dummy */
-	return 0;
-}
-
-size_t HOGEP_HASH(Hoge *x)
-{
-	/* dummy */
-	return 0;
-}
+size_t HOGE_HASH(Hoge x);
+size_t HOGEP_HASH(Hoge *x);
 
 
 #endif
